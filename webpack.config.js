7f6da313230742596657f4cf373de4c797f7636d
vingojw@gmail.com <vingojw@gmail.com>
2017-03-23 11:29:28 +0800

    u

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');//webpack中生成HTML的插件
-var ExtractTextPlugin = require('extract-text-webpack-plugin');//提取单独的css
-var path = require('path');
-var glob = require('glob');
-var shell = require('shelljs');
-var moment = require('moment');
+//是否生产
 var production = process.env.NODE_ENV == 'production';
-var entryNameTime = moment().format("YYYYMMDDhhmmss");
-shell.rm('-rf','./dist');
-//shell.cp('-R', 'stuff/', 'out/Release');
-var publicPath = '/';
+
+var path = require('path');
+var webpack = require('webpack');
+
+//筛选目录文件
+var glob = require('glob');
+
+//webpack中生成HTML的插件
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//删除目录
+var CleanPlugin = require("clean-webpack-plugin");     
+
+//提取单独的css
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+//配置
+var entryNameTime = production?require('moment')().format("YYYYMMDDhhmmss"):'';
+var publicPath = production?'http://127.0.0.1:8080/test/':'/';
+
+
+//拷贝资源可以使用   CopyWebpackPlugin 拷贝
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var extractCSS = new ExtractTextPlugin({
 	filename:'style/'+entryNameTime + '_[name].css',
 	allChunks:true  // 解决，编译完成后访问， Cannot read property call of undefined 地址： https://segmentfault.com/q/1010000007045505/a-1020000007049825
@@ -55,7 +69,12 @@ var configs = {
 		]
 	},
 	plugins: [
-		new webpack.ProvidePlugin({ //载入jq,这样就不用每个里面都require了，直接使用  $
+		new CopyWebpackPlugin(
+			[
+				{from:'./src/copy',to:'copy'}//在 npm run build 的时候，将copy文件复制到根目录下的copy
+			]),
+		new CleanPlugin(['dist']),
+		new webpack.ProvidePlugin({
 			$: 'jquery',
 			jQuery: 'jquery'
 		}),
@@ -77,9 +96,7 @@ if(!production){
 	configs.plugins.push(new webpack.HotModuleReplacementPlugin());//保存文件就会刷新页面
 }
 
-
 module.exports = configs;
-
 
 /*获取所有实体*/
 function getEntries(){
@@ -110,7 +127,6 @@ function getEntries(){
 			htmlPlugins.push(new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
 				favicon: './src/css/images/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
 				filename: './' + name + '.html',
-				title:'123',
 				template: './src/' + name + '.html', //html模板路径
 				inject: true, //js插入的位置，true/'head'/'body'/false
 				chunks: ['vendors', name],//需要引入的chunk，不配置就会引入所有页面的资源
